6993f63201fbbbb8e429564a3cc65f06a4a81c9e
fmmsilva <fmmsilva@gmail.com>
2014-09-25 19:01:00 -0300

    Revert "Revert "Inclusão do arquivo UrlExtensions""
    This reverts commit 028a20bfb579388fc4e7102d74d2c428fc58c1a1.

---
+++ System/Web/Mvc/UrlExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Dynamic;
+using System.Web.Routing;
+
+
+namespace System.Web.Mvc
+{
+    public static class UrlExtensions
+    {
+
+        public static string Action<TController>(this UrlHelper urlHelper, Expression<Func<TController, object>> actionExpression)
+        {
+            Type controller = typeof(TController);
+            string controllerName = controller.Name.Replace("Controller", "");
+
+            LambdaExpression action = actionExpression;
+            MethodCallExpression body = action.Body as MethodCallExpression;           
+            MethodInfo method = body.Method;
+            string actionName = method.Name;
+
+            ParameterInfo[] parameters = method.GetParameters();
+
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            foreach (ConstantExpression argument in body.Arguments)
+            {
+                int i = body.Arguments.IndexOf(argument);
+                string key = parameters[i].Name;
+                object value = argument.Value;               
+                dict.Add(key, value);
+            }
+
+            RouteValueDictionary routeDict = null;
+            if (dict.Count() > 0)
+            {
+                 routeDict = new RouteValueDictionary(dict);
+            }
+            return urlHelper.Action(actionName, controllerName, routeDict);
+        }
+
+
+
+    }
+
+
+}
